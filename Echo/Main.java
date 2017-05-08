a227669cd7bfa19c09e9081f4df0a5f7a7a5a8ef
Aidan Handa <AKHanda@student.naperville203.org>
2017-05-08 13:48:31 -0500

    Got STarted

---
+++ Echo/Main.java
@@ -0,0 +1,9 @@
+
+public class Main
+{
+    // instance variables - replace the example below with your own
+    public static void main(String[] args) {
+        MotorControl mc = new MotorControl();
+        
+    }
+}
