9f6b7dd214c83d43541b1e64606b7134a5fc28da
Aidan Handa <AKHanda@student.naperville203.org>
2017-05-08 14:08:22 -0500

    Working on listeners

--- Echo/Main.java
+++ Echo/Main.java
@@ -3,7 +3,9 @@ public class Main
 {
     // instance variables - replace the example below with your own
     public static void main(String[] args) {
-        MotorControl mc = new MotorControl();
-        
+        //MotorControl mc = new MotorControl();
+        ConstantUltrasonic cu = new ConstantUltrasonic();
+        cu.setDaemon(true); 
+        cu.start();
     }
 }
