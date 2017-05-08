4348876c451027846af6180997fbd7409065c065
alex-nordman <alnordyman@gmail.com>
2017-05-08 14:06:54 -0500

    Started up MotorControl class

---
+++ Echo/MotorControl.java
@@ -0,0 +1,27 @@
+import lejos.nxt.Motor;
+
+/**
+ * Write a description of class MotorControl here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MotorControl
+{
+    public MotorControl()
+    {
+    }
+    public static void turnLeft()
+    {
+        Motor.A.forward();
+    }
+    public static void turnRight()
+    {
+    }
+    public static void forward()
+    {
+    }
+    public static void backward()
+    {
+    }
+}
