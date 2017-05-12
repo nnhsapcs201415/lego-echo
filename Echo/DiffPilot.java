46402a72e1fa77c9978624813e65e14ea09a61f7
alex-nordman <alnordyman@gmail.com>
2017-05-12 14:11:44 -0500

    Differential Pilot BABYYYY

---
+++ Echo/DiffPilot.java
@@ -0,0 +1,22 @@
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.RegulatedMotorListener;
+
+/**
+ * Write a description of class DifferentialPilot here.
+ * 
+ * @author alnordman
+ * @version 5/12/2017
+ */
+public abstract class DiffPilot implements RegulatedMotorListener
+{
+    public static void rotationStarted()
+    {
+    }
+    
+    public static void rotaionStopped()
+    {
+    }
+    
+
+}
