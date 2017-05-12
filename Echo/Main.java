dedbe2bef6af9ee87a761f890e40748b4fbadb84
ryguan <chessleoperd@gmail.com>
2017-05-12 13:51:53 -0500

    adding Main

--- Echo/Main.java
+++ Echo/Main.java
@@ -5,12 +5,12 @@ public class Main
 {
     // instance variables - replace the example below with your own
     public static void main(String[] args) {
-        //MotorControl mc = new MotorControl();
-//         ObjectDetect listener = new ObjectDetect();
+MotorControl mc = new MotorControl();
+        ObjectDetect listener = new ObjectDetect();
         UltrasonicSensor us = Sensors.UC;
-//         RangeFeatureDetector fd = new RangeFeatureDetector(us, ObjectDetect.MAX_DETECT, 500);
-//         fd.addListener(listener);
-//         Button.ENTER.waitForPressAndRelease();
+        RangeFeatureDetector fd = new RangeFeatureDetector(us, ObjectDetect.MAX_DETECT, 500);
+        fd.addListener(listener);
+        Button.ENTER.waitForPressAndRelease();
         UltrasoundShi u = new UltrasoundShi(us);
         u.sendout();
         
