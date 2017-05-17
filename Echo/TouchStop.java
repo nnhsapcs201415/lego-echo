013379c35758042fb911e33fd28e311ea6731f53
Aidan Handa <AKHanda@student.naperville203.org>
2017-05-17 14:23:47 -0500

    setup for ryan

---
+++ Echo/TouchStop.java
@@ -0,0 +1,9 @@
+import lejos.nxt.SensorPortListener;
+import lejos.nxt.SensorPort;
+public class TouchStop implements SensorPortListener{
+    
+    public void stateChanged(SensorPort aSource, int aOldValue, int aNewValue) {
+        
+    }
+}
+
